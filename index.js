91967c3c44a2aebfd9e5b0c87a264574c9d33fad
NArpi100 <109304286+NArpi100@users.noreply.github.com>
2023-08-28 00:41:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ alert("Happy Birthday");
 
 $("h4").css("color","powderblue");
 $("h1").css("color","#ff4c17");
-//$("p").css("color","#ff4c17");
+$("p").css("color","#008080");
 $("h2").css("color","#900c3f");
 $("h1").click(function(){
   $("i").css("color","yellow");
