5ae2c2faec915e628c29f2d6e3c32983644515f2
NArpi100 <109304286+NArpi100@users.noreply.github.com>
2024-06-14 11:05:09 +0530

    Update this JS file.

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ alert("Happy Birthday");
 $("h4").css("color","powderblue");
 $("h1").css("color","#fff");
 $("p").css("color","#008080");
-$("h2").css("color","gold");
+$("h2").css("color","#dc143c");
 $("h1").click(function(){
   $("i").css("color","pink");
 });
