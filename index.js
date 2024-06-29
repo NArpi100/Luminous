a770f7d4eec5f0ef68a8cddb4ce3c9c7a39055e2
NArpi100 <109304286+NArpi100@users.noreply.github.com>
2024-06-29 20:33:07 +0530

    Update index.js file

--- index.js
+++ index.js
@@ -17,3 +17,29 @@ $(document).keypress(function(event){
 $("button").css("background-color","#008080");
 $("button").css("color","#fff");
 
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", " ");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
