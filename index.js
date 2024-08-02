05b99a50dc969977aa77450757d6d55025c7941b
Arpita <109304286+NArpi100@users.noreply.github.com>
2024-08-02 19:04:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,7 @@ $("h4").css("color","powderblue");
 $("h1").css("color","#e4d00a");
 $("p").css("color","#008080");
 $("h2").css("color","#dc143c");
-$("h1").click(function(){
-  $("i").css("color","#f5ba13");
-});
+
 
 $(document).keypress(function(event){
   $("h4").text(event.key);
