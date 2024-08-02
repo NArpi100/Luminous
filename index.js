18f67fb1d096e6383e0802ad8f62a70ad9bf34ae
Arpita <109304286+NArpi100@users.noreply.github.com>
2024-08-02 19:22:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,6 @@ alert("HELLO");
 alert("Navin 💌");
 
 
-$("h4").css("color","powderblue");
-$("h1").css("color","#e4d00a");
-$("p").css("color","#008080");
-$("h2").css("color","#dc143c");
 
 
 $(document).keypress(function(event){
